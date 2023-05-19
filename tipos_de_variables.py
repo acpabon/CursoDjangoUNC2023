7c98989a93a4d77df12d634830e84d198b2e7406
Andres Pabon <ing.andresp02@gmail.com>
2023-05-18 19:26:45 -0500

    tipos_de_varibles

---
+++ tipos_de_variables.py
@@ -0,0 +1,11 @@
+edad = 29
+print("Edad: " + str(edad) + " años")
+print(type(edad))
+
+altura = 1.8
+print("Altura: " + str(altura) + " metros")
+print(type(altura))
+
+nombre = "Andres Camilo Pabón Aguilar"
+print("Nombre: " + nombre)
+print(type(nombre))
