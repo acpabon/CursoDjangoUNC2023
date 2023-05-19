0055867c3ca6a0244eff1b2ceee21e9e280e4a32
Andres Pabon <ing.andresp02@gmail.com>
2023-05-18 20:13:28 -0500

    ejercicio 2

---
+++ ejercicios_clase/listas_tuplas_diccionarios.py
@@ -0,0 +1,24 @@
+# lista de simbolos patrios
+simbolos = ['Himno nacional', 'Bandera', 'Orquidea', 'Palma de cera', 'Condor andino', 'Sombreo vueltiao']
+
+# tupla de colores de la bandera
+colores = ('amarillo', 'azul', 'rojo')
+
+# diccionario de datos del pais
+datos = {
+    "Gentilicio": 'Colombiano',
+    "poblacion": '52215503',
+    "idioma": 'español',
+    "dominio": '.co'
+}
+
+print(simbolos)
+print(type(simbolos))
+print()
+
+print(colores)
+print(type(colores))
+print()
+
+print(datos)
+print(type(datos))
---
+++ ejercicios_clase/tipos_de_variables.py
@@ -0,0 +1,11 @@
+edad = 29
+print("Edad: " + str(edad) + " años")
+print(type(edad))
+
+altura = 1.8
+print("Altura: " + str(altura) + " metros")
+print(type(altura))
+
+nombre = "Andres P"
+print("Nombre: " + nombre)
+print(type(nombre))
